dc63f13d4901785a59988d9f0405c638758f6795
Felix Lindemann <felixlindemann@web.de>
2014-02-12 00:07:21 +0100

    Test updated: Objects within testthat are invisible.

--- inst/tests/testhnulink.r
+++ inst/tests/testhnulink.r
@@ -9,16 +9,13 @@ n2<- HNUNode.create(x=13, y=24)
 n3<- new("HNUNode", x=7, y=16)
 n4<- new("HNUNode", x=7, y=20)
 
-context("\tTest 01: Are Objects created correctly?") 
-test_that("Test for creating objects without Messages", {
+#create links
+#
 	l1 <- HNULink.create(n1,n2)
 	l2 <- HNULink.create(n1,n3)
 	l3 <- HNULink.create(n1,n4)
 	l4 <- HNULink.create(n3,n4)
- 
-})
-
-context("\tTest 02: Are the Node-Methods for Links working?") 
+context("\tTest 01: Are the Node-Methods for Links working?") 
 test_that("Node-Methods work for Links", {
 
 	expect_true(l1$distance == 5)
