c67d3d38c723179a8aa94091c1c26050bc545089
Felix Lindemann <felixlindemann@web.de>
2014-02-14 11:08:32 +0100

    TPP: Northwest Corner Rule implemented

---
+++ R/HNU.OR.TPP.NorhtwestCornerRule.r
@@ -0,0 +1,29 @@
+setGeneric("HNU.OR.TPP.NW",  function(object,...)  standardGeneric("HNU.OR.TPP.NW") )
+ setMethod("HNU.OR.TPP.NW", signature(object="HNUGeoSituation"),
+  function(object,...){ 
+    
+    li<-list(...)  
+  	object <- HNU.OR.TPP.Prepare(object, ...) 			# repair degenerated if needed
+	object <- HNU.OR.TPP.addTransportplan(object, ...)  # set initial Transportation Plan
+	
+	#set supply and demand
+  	supply <- sapply(object$warehouses, function(o){o$supply})
+	demand <- sapply(object$customers , function(o){o$demand})
+
+	#set initial transportplan
+	x <- object$transportplan * 0 
+
+
+	for(i in (1:length(object$warehouses))){ 
+		for(j in (1:length(object$customers))){
+			x[i,j] <- min(c(supply[i],demand[j]))
+			supply[i] <- supply[i] - x[i,j]
+			demand[j] <- demand[j] - x[i,j] 
+		}
+	} 
+
+	object$transportplan <- x  
+
+	return(object)
+	}
+)
